471724f3053b2e33c48f6cf9ff2bbc7bc12b16ef
loyu <lito0210@outlook.com>
2022-11-15 19:09:57 +0800

    add interface auth ways

--- src/emailsend.rs
+++ src/emailsend.rs
@@ -25,50 +25,60 @@ pub struct EmailMessage {
     pub reciever: String,
     ///邮件发件人，与smtp一致
     pub sender: String,
+    ///api鉴权
+    pub tokens: String,
 }
+
+///用户名密码创建与注册
+
+///根据用户名密码创建token，并存储token的hash值
+pub async fn get_access_token() -> String {}
 
 pub async fn handle_emailmessage(
     Json(email_message): Json<EmailMessage>,
     async_pools_sender: Extension<AsyncSmtpTransport<Tokio1Executor>>,
 ) -> impl IntoResponse {
-    let mut return_response = String::new();
+    if email_message.tokens == "tokens" {
+        let mut return_response = String::new();
+        //一个具有多个所有者的可变引用
+        let recv = email_message.reciever.clone();
+        // let rep = RefCell::new(return_response);
 
-    //一个具有多个所有者的可变引用
-    let recv = email_message.reciever.clone();
-    // let rep = RefCell::new(return_response);
+        let email = Message::builder()
+            .from(email_message.sender.parse().unwrap())
+            .reply_to(email_message.sender.parse().unwrap())
+            .to(email_message.reciever.parse().unwrap())
+            .subject(email_message.subject)
+            .body(email_message.body)
+            .unwrap();
+        let result = tokio::spawn(async move {
+            let result = async_pools_sender.send(email).await;
+            match result {
+                Ok(_) => {
+                    info!("Send email to {} successful", recv);
+                    let infomsg = format!("Send email to {} successful", recv);
 
-    let email = Message::builder()
-        .from(email_message.sender.parse().unwrap())
-        .reply_to(email_message.sender.parse().unwrap())
-        .to(email_message.reciever.parse().unwrap())
-        .subject(email_message.subject)
-        .body(email_message.body)
-        .unwrap();
-    let result = tokio::spawn(async move {
-        let result = async_pools_sender.send(email).await;
+                    return_response.push_str(&infomsg);
+                    return_response
+                }
+                Err(e) => {
+                    info!("Send email error: {}", e);
+                    let dd = format!("{:#?}", e);
+                    return_response.push_str(dd.as_str());
+                    return_response
+                }
+            }
+        })
+        .await;
         match result {
-            Ok(_) => {
-                info!("Send email to {} successful", recv);
-                let infomsg = format!("Send email to {} successful", recv);
-
-                return_response.push_str(&infomsg);
-                return_response
-            }
+            Ok(resp) => (StatusCode::ACCEPTED, resp),
             Err(e) => {
-                info!("Send email error: {}", e);
                 let dd = format!("{:#?}", e);
-                return_response.push_str(dd.as_str());
-                return_response
+                (StatusCode::ACCEPTED, dd)
             }
         }
-    })
-    .await;
-    match result {
-        Ok(resp) => (StatusCode::ACCEPTED, resp),
-        Err(e) => {
-            let dd = format!("{:#?}", e);
-            (StatusCode::ACCEPTED, dd)
-        }
+    } else {
+        (StatusCode::UNAUTHORIZED, "Token is not effective")
     }
 }
 
@@ -82,24 +92,30 @@ pub async fn generate_smtpclient_pool(
             .unwrap()
             .as_str(),
     )
+    .to_owned()
     .unwrap();
-    let token = values
-        .get("smtpserver")
-        .unwrap()
-        .get("token")
-        .unwrap()
-        .as_str()
-        .unwrap()
-        .to_string();
-    let user = values
-        .get("smtpserver")
-        .unwrap()
-        .get("user")
-        .unwrap()
-        .as_str()
-        .unwrap()
-        .to_string();
 
+    // let valueParse: &dyn Fn(&str, &str) -> &str = &|x: &'static str, y: &'static str| -> &str {
+    //     values
+    //         .get(x)
+    //         .expect("can't find values")
+    //         .get(y)
+    //         .expect("can't find values of the config file")
+    //         .as_str()
+    //         .unwrap()
+    // };
+
+    let valueParse = |x: &'static str, y: &'static str| -> &str {
+        values
+            .get(x)
+            .expect("can't find values")
+            .get(y)
+            .expect("can't find values of the config file")
+            .as_str()
+            .unwrap()
+    };
+    let token = valueParse("smtpserver", "token").into();
+    let user = valueParse("smtpserver", "user").into();
     info!("Starting connecting {}", &user);
     let pools = PoolConfig::new().min_idle(0).max_size(10);
     let async_pools_sender: AsyncSmtpTransport<Tokio1Executor> =
